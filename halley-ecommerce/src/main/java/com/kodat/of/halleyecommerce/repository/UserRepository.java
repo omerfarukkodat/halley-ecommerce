a9fec2b4517818a37ca41484f32a10f6072e3761
Faruk Kodat <farukkodat@gmail.com>
2024-09-20 23:21:38 +0300

    userDetailService implemented

--- halley-ecommerce/src/main/java/com/kodat/of/halleyecommerce/repository/UserRepository.java
+++ halley-ecommerce/src/main/java/com/kodat/of/halleyecommerce/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.kodat.of.halleyecommerce.repository;
 import com.kodat.of.halleyecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+    Optional<User> findByEmail(String userEmail);
 }
---
+++ halley-ecommerce/src/main/java/com/kodat/of/halleyecommerce/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kodat.of.halleyecommerce.security;
+
+import com.kodat.of.halleyecommerce.entity.CustomUserDetails;
+import com.kodat.of.halleyecommerce.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException(userEmail));
+        return new CustomUserDetails(user);
+    }
+}
