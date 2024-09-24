289f3dbd5cbee1e5c938abe78c128a05190fbbc3
Faruk Kodat <farukkodat@gmail.com>
2024-09-24 17:08:56 +0300

    first logging to CategoryService before the custom exceptions

--- halley-ecommerce/src/main/java/com/kodat/of/halleyecommerce/category/CategoryServiceImpl.java
+++ halley-ecommerce/src/main/java/com/kodat/of/halleyecommerce/category/CategoryServiceImpl.java
@@ -2,11 +2,15 @@ package com.kodat.of.halleyecommerce.category;
 
 import com.kodat.of.halleyecommerce.dto.category.CategoryDto;
 import com.kodat.of.halleyecommerce.mapper.category.CategoryMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CategoryServiceImpl.class);
+
     private final CategoryRepository categoryRepository;
 
     public CategoryServiceImpl(CategoryRepository categoryRepository) {
@@ -16,23 +20,33 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryDto addParentCategory(CategoryDto categoryDto , Authentication connectedAdmin) {
-       verifyAdminRole(connectedAdmin); // Check admin role
+        LOGGER.info("Adding parent category with name: {}", categoryDto.getCategoryName());
+
+        verifyAdminRole(connectedAdmin); // Check admin role
         validateCategoryName(categoryDto.getCategoryName()); // Check categoryName added before?
         Category category = CategoryMapper.toCategory(categoryDto,null);
-       return CategoryMapper.toCategoryDto(categoryRepository.save(category));
+        LOGGER.info("Parent category added successfully with ID: {}", category.getId());
+
+        return CategoryMapper.toCategoryDto(categoryRepository.save(category));
     }
 
     private void verifyAdminRole(Authentication connectedAdmin) {
         if (connectedAdmin.getAuthorities().stream()
                 .noneMatch(authority -> authority.getAuthority().equals("ADMIN"))){
+            LOGGER.error("Unauthorized access attempt by user: {}", connectedAdmin.getName());
             throw new SecurityException("You are not an admin");
         }
+        LOGGER.info("Admin role verified for user: {}", connectedAdmin.getName());
+
     }
 
     private void validateCategoryName(String categoryName) {
         if (categoryRepository.existsByCategoryName(categoryName)){
+            LOGGER.warn("Attempt to add existing category: {}", categoryName);
             throw new RuntimeException("Category: "+ categoryName + " already exists");
         }
+        LOGGER.info("Category name validation passed for: {}", categoryName);
+
     }
 
 
